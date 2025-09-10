46c4e3654f8ab8e9bc29ac133b0cf9f1432c73ea
srilekha2452 <srilekha3890@gmail.com>
2025-09-10 19:46:06 +0530

    redirect to login page if not auth

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   const user=useSelector((store)=>store.user)
@@ -7,7 +8,7 @@ const NavBar = () => {
     <div>
       <div className="navbar bg-base-300 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">devTinder</a>
+    <Link to="/"className="btn btn-ghost text-xl">devTinder</Link>
   </div>
   <div className="flex gap-3 items-center">
     {user && <span className="text-md font-semibold items-center">Welcome {user.firstName}</span>}
@@ -24,10 +25,10 @@ const NavBar = () => {
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li>
-          <a className="justify-between">
+          <Link to="/profile"className="justify-between">
             Profile
             <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
